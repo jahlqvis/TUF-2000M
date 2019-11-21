3f36e3062139b61e98ee07ef101fd9f374d7bf80
Johan <jahlqvis@gmail.com>
2019-11-21 23:58:26 +0200

    Added reader class

--- TUF-2000M/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace TUF_2000M
-{
-    public class Class1
-    {
-    }
-}
---
+++ TUF-2000M/Reader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace TUF_2000M
+{
+    public static class Reader
+    {
+        static StreamReader URLStream(String fileurl)
+        {
+            return new StreamReader(new HttpClient().GetStreamAsync(fileurl).Result);
+        }
+    }
+    
+}
