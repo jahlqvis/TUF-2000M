587f69a487cc80e9363ebf51160551d4029bdefb
Johan <jahlqvis@gmail.com>
2019-11-22 22:44:18 +0200

    Added ReadNthLine test

--- TUF-2000M.Test/UnitTests.cs
+++ TUF-2000M.Test/UnitTests.cs
@@ -27,5 +27,21 @@ namespace TUF_2000M.Test
 
             Assert.AreEqual("2018-08-03 04:06", str);
         }
+
+        [DataTestMethod]
+        [DataRow(1, "2018-08-03 04:06")]
+        [DataRow(22, "21:64432")]
+        [DataRow(64, "63:0")]
+        [DataRow(101, "100:17503")]
+        public void TestReadUrlAndNthLine(int lineNr, string expected)
+        {
+            Reader r = new Reader();
+            bool result = false;
+            result = r.ReadURL("http://tuftuf.gambitlabs.fi/feed.txt");
+
+            string str = r.GetLine(lineNr);
+
+            Assert.AreEqual(expected, str);
+        }
     }
 }
